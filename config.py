8b1d7a0381fc11222cb4396258bcbc366c2a0fc9
yu.wang <zed31501591@gmail.com>
2022-05-28 13:26:55 +0800

    bert

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ tag = pwd.split('/')[1]
 if tag=='lustre':
     DATA_DIR = '/lustre/home/acct-aemwx/aemwx-user1/wangyu/my/nlp-final/data'
 elif tag=='dssg':
-    DATA_DIR = '/dssg/home/acct-aemwx/aemwx-user1/wangyu/my/nlp-final/data'
+    DATA_DIR = '/dssg/home/acct-aemwx/aemwx-user1/wangyu/my/raw/data'
 else:
     DATA_DIR='/Users/zed/workspace/VSCode/SJTU/nlp_proj/data'
--- notebook/pl/bertcn_rls.py
+++ notebook/pl/bertcn_rls.py
@@ -10,7 +10,6 @@ from pytorch_lightning import loggers as pl_loggers
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import  ModelCheckpoint, EarlyStopping
 import transformers
-from nlp import load_dataset
 import torchmetrics as tm
 from pytorch_lightning.loggers import WandbLogger
 import wandb
@@ -25,21 +24,10 @@ warnings.filterwarnings('ignore')
 import config
 RANDOM_SEED = 999
 pl.seed_everything(RANDOM_SEED)
+BERT_MODEL_NAME = './pre_train/bert-base-chinese'
 
 
-df_pos = pd.read_excel(config.DATA_DIR+'/女权无标点.xlsx', index_col=0)
-df_neg = pd.read_excel(config.DATA_DIR+'/非女权无标点.xlsx', index_col=0)
-
-df_pos['target'] = 1
-df_neg['target'] = 0
-df = pd.concat([df_pos, df_neg], axis=0)
-df.rename({0: 'reviews'}, inplace=True, axis=1)
-
-df_insmp, df_test = train_test_split(df, test_size=0.2, shuffle=True)
-df_train, df_valid = train_test_split(df_insmp, test_size=0.1, shuffle=False)
-
-
-class ImdbDataset(Dataset):
+class MyDataSet(Dataset):
     def __init__(self, notes, targets, tokenizer, max_len):
         self.notes = notes
         self.targets = targets
@@ -74,20 +62,7 @@ class ImdbDataset(Dataset):
         }
         
         
-BERT_MODEL_NAME = 'bert-base-chinese'
-tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_NAME)
-train_dataset = ImdbDataset(notes=df_train['reviews'],
-    targets=df_train['target'],
-    tokenizer=tokenizer,
-    max_len=512)
-val_dataset = ImdbDataset(notes=df_valid['reviews'],
-    targets=df_valid['target'],
-    tokenizer=tokenizer,
-    max_len=512)
-test_dataset = ImdbDataset(notes=df_test['reviews'],
-    targets=df_test['target'],
-    tokenizer=tokenizer,
-    max_len=512)
+
 
 class MyDataModule(pl.LightningDataModule):
 
@@ -95,7 +70,7 @@ class MyDataModule(pl.LightningDataModule):
         super().__init__()
         self.batch_size = batch_size
         self.num_workers = num_workers
-        self.num_classes = 2
+        self.num_classes = 1
         self.train = train
         self.val = val
         self.test = test
@@ -110,8 +85,8 @@ class MyDataModule(pl.LightningDataModule):
     def test_dataloader(self):
         return DataLoader(self.test, batch_size=self.batch_size)
     
-datamodule = MyDataModule(train_dataset,val_dataset,test_dataset)
-class ImdbModel(pl.LightningModule):
+
+class BERTModel(pl.LightningModule):
 
     def __init__(self,
                  learning_rate: float = 0.0001 * 8,
@@ -127,11 +102,9 @@ class ImdbModel(pl.LightningModule):
             BERT_MODEL_NAME, return_dict=True)
         self.pre_classifier = torch.nn.Linear(self.bert.config.hidden_size, self.bert.config.hidden_size)
         self.classifier = torch.nn.Linear(self.bert.config.hidden_size, self.num_labels)
-        self.acc = tm.Accuracy()
         # self.dropout = torch.nn.Dropout(self.bert.config.seq_classif_dropout)
         self.dropout=torch.nn.Dropout(0.2)
         # relu activation function
-        self.sm = torch.nn.Sigmoid()
         self.relu = torch.nn.ReLU()
     def forward(self, input_ids, attention_mask, labels):
       
@@ -144,7 +117,6 @@ class ImdbModel(pl.LightningModule):
         pooled_output = self.relu(pooled_output)  # (bs, dim)
         pooled_output = self.dropout(pooled_output)  # (bs, dim)
         logits = self.classifier(pooled_output)  # (bs, dim)
-        logits = self.sm(logits)
         return logits
     
     def get_outputs(self, input_ids, attention_mask):
@@ -162,14 +134,14 @@ class ImdbModel(pl.LightningModule):
         attention_mask = batch['attention_mask']
         #token_type_ids = batch['token_type_ids']
         # fwd
-        y_hat = self(input_ids, attention_mask, label)
-        
+        y_hat = self(input_ids, attention_mask, label).view(-1,self.num_labels)
+
         # loss
         loss_fct = torch.nn.CrossEntropyLoss()
-        loss = loss_fct(y_hat.view(-1, self.num_labels), label.view(-1))
+        loss = loss_fct(y_hat, label)
         #loss = F.cross_entropy(y_hat, label)
-        self.log("train_loss", loss)
-        self.log('train_acc',self.acc(y_hat.view(-1, self.num_labels), label.view(-1)))
+        self.log_dict({"train_loss":loss})
+        return {'loss':loss}
 
     def validation_step(self, batch, batch_nb):
         # batch
@@ -179,34 +151,29 @@ class ImdbModel(pl.LightningModule):
         #token_type_ids = batch['token_type_ids'] 
         # fwd
         y_hat = self(input_ids, attention_mask, label)
-        
         # loss
         #loss = F.cross_entropy(y_hat, label)
         loss_fct = torch.nn.CrossEntropyLoss()
-        loss = loss_fct(y_hat.view(-1, self.num_labels), label.view(-1))
+        loss = loss_fct(y_hat, label)
 
         # acc
-        a, y_hat = torch.max(y_hat, dim=1)
-        val_acc = self.acc(y_hat, label)
-        self.log('val_loss',loss)
-        self.log('val_acc', val_acc)
+        a, y_hat = torch.max(y_hat, dim=1) # y_hat = [1,0,0,1,...] [N]
+        val_acc = accuracy_score(y_hat.cpu().detach().numpy(), label.cpu().detach().numpy())
         
+        self.log_dict({'val_loss':loss,
+                  'val_acc':val_acc})
+        return {'val_loss':torch.tensor([loss]),'val_acc':torch.tensor([val_acc])}
         
 
-    # def validation_epoch_end(self, outputs):
-    #     return None
-    #     avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-    #     avg_val_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+        avg_val_acc = torch.stack([x['val_acc'] for x in outputs]).mean()
         
-    #     # logs
-    #     self.log('avg_val_loss',avg_loss,'avg_val_acc',avg_val_acc)
+        # logs
+        self.log_dict({'avg_val_loss':avg_loss,'avg_val_acc':avg_val_acc})
      
     
     def on_batch_end(self):
-        #for group in self.optim.param_groups:
-        #    print('learning rate', group['lr'])
-        # This is needed to use the One Cycle learning rate that needs the learning rate to change after every batch
-        # Without this, the learning rate will only change after every epoch
         if self.sched is not None:
             self.sched.step()
     
@@ -223,48 +190,71 @@ class ImdbModel(pl.LightningModule):
         
         # loss
         loss_fct = torch.nn.CrossEntropyLoss()
-        loss = loss_fct(y_hat.view(-1, self.num_labels), label.view(-1))
+        loss = loss_fct(y_hat, label)
         
         a, y_hat = torch.max(y_hat, dim=1)
-        test_acc = self.acc(y_hat, label)
+        test_acc = accuracy_score(y_hat.cpu().detach().numpy(), label.cpu().detach().numpy())
         self.log('test_acc',test_acc)
+        return {'test_loss':torch.tensor([loss]),'test_acc':torch.tensor([test_acc])}
+    def test_epoch_end(self, outputs):
+        avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
+        avg_test_acc = torch.stack([x['test_acc'] for x in outputs]).mean()
 
-    # def test_epoch_end(self, outputs):
-    #     return None
-    #     avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
-    #     avg_test_acc = torch.stack([x['test_acc'] for x in outputs]).mean()
-
-    #     tensorboard_logs = {'avg_test_loss': avg_loss, 'avg_test_acc': avg_test_acc}
-    #     self.log('avg_test_loss',avg_loss,'avg_test_acc',avg_test_acc)
+        self.log_dict({'avg_test_loss':avg_loss,'avg_test_acc':avg_test_acc})
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.lr)
         
         
 def main():
-
     """ Main training routine specific for this project. """
-
     wandb.login()
-    wandb_logger = WandbLogger(project='bert-test')
-    model = ImdbModel(learning_rate=1e-3)
+    wandb_logger = WandbLogger(project='bert-test',save_dir='./wnb_logs/',offline=False)
+    
+    df_pos = pd.read_excel(config.DATA_DIR+'/女权无标点.xlsx', index_col=0)
+    df_neg = pd.read_excel(config.DATA_DIR+'/非女权无标点.xlsx', index_col=0)
+
+    df_pos['target'] = 1
+    df_neg['target'] = 0
+    df = pd.concat([df_pos, df_neg], axis=0)
+    df.rename({0: 'reviews'}, inplace=True, axis=1)
+    df['reviews'] = df.apply(lambda i: i.reviews.replace('女权','').replace('女性',''),axis=1)
+    df_insmp, df_test = train_test_split(df, test_size=0.2, shuffle=True)
+    df_train, df_valid = train_test_split(df_insmp, test_size=0.1, shuffle=False)
+    
+    tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_NAME)
+    train_dataset,val_dataset,test_dataset =[
+        MyDataSet(notes=i['reviews'],
+        targets=i['target'],
+        tokenizer=tokenizer,
+        max_len=32) for i in [df_train,df_valid,df_test]
+    ]
     checkpoint_callback = ModelCheckpoint(
-        dirpath="checkpoints",
+        dirpath="./logs/checkpoints",
         filename="best-checkpoint",
         save_top_k=1,
         verbose=True,
         monitor="val_loss",
         mode="min"
     )
-    early_stopping_callback = EarlyStopping(monitor='val_loss', patience=20)
+    early_stopping_callback = EarlyStopping(
+        monitor='val_loss',
+        patience=50,
+        )
     trainer = pl.Trainer(
+        default_root_dir='./logs',
         logger=wandb_logger,
         checkpoint_callback=checkpoint_callback,
         callbacks=[early_stopping_callback],
-        max_epochs=100,
+        max_epochs=500,
+        gpus=4,
     )
+    datamodule = MyDataModule(train_dataset,val_dataset,test_dataset,batch_size=32)
+    model = BERTModel(learning_rate=1e-5,batch_size=32)
     trainer.fit(model,datamodule)
     trainer.test(model,datamodule)
     wandb.finish()
-if __name__=='__main__':
+    
+    
+if __name__=='__main__':   
     main()
